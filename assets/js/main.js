2120e4d133d3f461cf7628aa8c4566b88736e53d
mmar58 <rahmanapu118@gmail.com>
2024-07-06 13:48:31 +0600

    Added  bounding box images

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+var boundingBoxImage=document.getElementById("BoundingBoxImage")
+function BoundingBoxItemHoverEnter(){
+    boundingBoxImage.src="assets/image/BoundingBox/BoundingBoxIcon.gif"
+}
+function BoundingBoxItemHoverOut(){
+    boundingBoxImage.src="assets/image/BoundingBox/BoundingBoxIcon.jpg"
+}
