db28f63d8462079db03809993d762cfebb7f8e2d
mmar58 <rahmanapu118@gmail.com>
2026-01-17 15:45:18 +0600

    Updated build

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-var boundingBoxImage=document.getElementById("BoundingBoxImage")
-function BoundingBoxItemHoverEnter(){
-    boundingBoxImage.src="assets/image/BoundingBox/BoundingBoxIcon.gif"
-}
-function BoundingBoxItemHoverOut(){
-    boundingBoxImage.src="assets/image/BoundingBox/BoundingBoxIcon.jpg"
-}
